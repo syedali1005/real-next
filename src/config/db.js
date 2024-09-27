995289d6c8d223c96ba2106dd41e3336502ed0f3
Syed Ali Ahmed <syedaliahmad22@gmail.com>
2024-09-27 14:38:37 +0500

    db.js changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,20 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-<<<<<<< HEAD
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error: ${error.message}`);
-=======
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection failed:', error.message);
->>>>>>> 05fe745cf8f6c76b54faa504865f8246597d3e01
+        console.error(`MongoDB connection failed: ${error.message}`);
         process.exit(1); // Exit process with failure
     }
 };
